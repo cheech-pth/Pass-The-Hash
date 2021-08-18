6888b904980bb6bf190322d1275b2a15e7311c13
coreyo-git <corey.oconnor@live.com.au>
2021-08-18 23:07:15 +1000

    Moved header to it's own file added /includes/Files for static header

--- Blog.php
+++ Blog.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
 <body>
 
     <?php 
+    include "/includes/header.php";
+
     $hello = "Hello, world!";
-    echo "Hello";
+    $games = array("Runescape", "WoW", "LoL", "Game");
+
+    foreach ($games as $game) 
+    {
+        echo "$game <br>";
+    }
+
     echo $hello;
     ?>
 </body>
--- index.php
+++ index.php
@@ -1,61 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pass The Hash</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header class="header">
-        <nav class="flex flex-jc-space-between flex-ai-c">
-            <a href="/" class="header__logo">
-                <img src="source/header-desktop.png" alt="PassTheHash">
-            </a>
-            
-            <div class="header__links hide-for-mobile">
-                <ul>
-                    <li><a href="#">Docs</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="/Blog.php">Blog</a></li>
-                    <li><a href="#">Github</a>
-                        <ul class="dropdown-nav">
-                            <li><a href="https://github.com/coreyo-git">Coreyo-git</a></li>
-                            <li><a href="https://github.com/cheech-pth">Cheech-pth</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="https://discord.gg/utF8NmwxbE">Discord</a></li>
-                </ul>
-                
-            </div>
+<?php 
 
-            <a href="#" class="button hide-for-mobile">Donate to PTH</a>
+include "/includes/header.php";
 
-            <div class="drop_down hide-for-desktop">
-                <a href="#" id="dropdown_button" onclick="showMenu()" class="header__hamburger hide-for-desktop">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-
-                    <center class="hide-for-desktop">
-                        <!-- dropdown list items will show when we click the drop button --> 
-                        <div id="dropdown-content" class="hide-for-desktop">
-                            <a href="#">Docs</a>
-                            <a href="#">About</a>
-                            <a href="https://github.com/cheech-pth">Github</a>
-                            <a href="https://discord.gg/utF8NmwxbE">Discord</a>
-                            <a href="/Blog.php">Blog</a>
-                        </div>
-                     </center>
-                </a>
-            </div>
-
-        </nav>
-    </header>
+?>
     <section class="cards">
         <a href="#" class="card">
             <div class="card__image" style="background-image:url(https://media.discordapp.net/attachments/521595368736161794/840242311396851743/unknown.png);"></div>
