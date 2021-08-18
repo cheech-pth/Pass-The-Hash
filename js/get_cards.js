0b422b76ed97301ab62a07fa4ac41c6c9a3dff48
coreyo-git <corey.oconnor@live.com.au>
2021-08-19 09:41:16 +1000

    Added commenting to make it more clear

--- js/get_cards.js
+++ js/get_cards.js
@@ -1,15 +1,23 @@
+// Fetches the local JSON file 
 fetch('/MockData/cards.json')
 .then(function (response) {
+    // Returns the response data in json format
     return response.json();
 })
 .then(function (data) {
+    // Calls the appendData Function below
     appendData(data);
 })
+// Catches any errors fetching the JSON data
 .catch(function (err) {
     console.log('error: ' + err);
 });
+
+// Takes the data fetched as an argument
+// Loops through by length destructuring elements off the array
+// Creates a div per element and then appends the same element with each element
 function appendData(data) {
-    console.log(data);
+    //console.log(data);
 var mainContainer = document.getElementById("cards");
 
 for (var i = 0; i < data.length; i++) {
