8ff264cec9827e3093c9cf66a42b0c6359012eaf
cheech-pth <michael.gogarty@gmail.com>
2021-08-25 20:53:17 +1000

    Fixed footer on mobile view

--- includes/footer.php
+++ includes/footer.php
@@ -11,6 +11,6 @@
 <footer>
     <div class="footer-container">
         <img src="source/logo.svg" alt="Pass The Hash Logo" class="img-pth-logo">
-        <p class="footer-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquid et perferendis accusamus facere? Ab delectus, doloremque impedit vel a officia exercitationem mollitia, inventore blanditiis unde iusto fuga facilis eligendi.</p>
+        <p class="footer-text">&copy; <?php echo 'Copyright ' . date("Y"); ?></p>
     </div>
 </footer>
