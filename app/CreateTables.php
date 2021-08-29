cb926cffe4772e8f7dd07346a2c716e4fb9f1d5e
coreyo-git <corey.oconnor@live.com.au>
2021-08-29 10:51:01 +1000

    CreateTables: Added User columns, user_details, lookup table for titles and more blog info

--- app/CreateTables.php
+++ app/CreateTables.php
@@ -9,11 +9,42 @@ class CreateTable {
     }
     # List of sql statements TODO: Loop if extras are added, build function anyway?
     public function createTable() {
-        $sqlList = ['CREATE TABLE IF NOT EXISTS blogs (
-            id INTEGER PRIMARY KEY,
-            author VARCHAR(100) NOT NULL, # TODO: will be foreign key for users eventually
-            title VARCHAR(500) NOT NULL,
-            body TEXT NOT NULL
+        $sqlList = [
+            'CREATE TABLE IF NOT EXISTS user (
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title INTEGER NOT NULL,
+                username VARCHAR(50) NOT NULL,
+                email VARCHAR(256) NOT NULL,
+                about VARCHAR(500),
+                pass_salt VARCHAR NOT NULL,
+                pass_hash VARCHAR NOT NULL,
+                created DATETIME NOT NULL,
+                total_posts INTEGER,
+                FOREIGN KEY (title) REFERENCES title_lookup(id)
+                );',
+
+            // Lookup table for user titles e.g. Admin, User, Moderator etc
+            'CREATE TABLE IF NOT EXISTS title_lookup (
+                id INTEGER PRIMARY KEY,
+                title VARCHAR(50) NOT NULL
+            );',
+
+            // Stores historical data of users
+            'CREATE TABLE IF NOT EXISTS user_detail (
+                user_id INTEGER,
+                last_login DATETIME,              
+                login_ip INET,
+                location VARCHAR
+                );',
+
+            'CREATE TABLE IF NOT EXISTS blog (
+                blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                author INTEGER NOT NULL,
+                title VARCHAR(256) NOT NULL,
+                intro TEXT NOT NULL, 
+                body TEXT NOT NULL,
+                summary TEXT NOT NULL,
+                FOREIGN KEY (author) REFERENCES user(user_id)
         );'];
 
         #loops through sqlList
